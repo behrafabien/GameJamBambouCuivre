8d920936e6f779879f58626ba575f5f569d0ab01
LucaDeslot <deslot.luca@gmail.com>
2018-02-05 15:24:55 +0100

    ajout de chargerCartes pas fonctionnel

--- main.rb
+++ main.rb
@@ -1,7 +1,35 @@
-require_relative 'window.rb'
 
-WindowWidth = 1280
-WindowHeight = 640
 
-window = Window.new(WindowWidth, WindowHeight)
-window.show
+
+def chargerCarte
+  array = []
+  File.open("cartes.txt") do |file|
+  data = file.readlines
+
+  data.each do |line|
+    line.split.each do |word|
+      array.push(word)
+    end
+  end
+end
+
+for i in 0..array.length()-1
+  puts array[i]
+  case array[i]
+  when "id"
+    id = array[i+1]
+  when "desc"
+    desc = ""
+    k = i+1
+    while array[k] != "ch1"
+      desc.to_s + array[k].to_s
+      k = k+1
+    end
+    puts "@@@@@@@@@@@@@@@"
+    puts desc
+  end
+end
+end
+
+
+chargerCarte()
