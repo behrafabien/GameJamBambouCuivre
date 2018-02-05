7055d5d9e90bacf5891b0231308a1d42fe89d09e
adrienprat <prat.adrien@orange.fr>
2018-02-05 16:26:41 +0100

    cartes.rb

--- cartes.rb
+++ cartes.rb
@@ -1,10 +1,10 @@
 class Cartes
   attr_reader :id, :desc, :choix, :consequence
-  def initialize
+  def initialize(id, desc, choix, consequence)
         @id = id
         @desc = desc
-        @choix = choix
-        @consequence = consequence
+        @choix = []
+        @consequence = []
   end
   def id=(new_id)
         @id = new_id
@@ -13,9 +13,9 @@ class Cartes
         @desc = new_desc
   end
   def choix=(new_choix)
-        @choix = new_choix
+        @choix = new_choix[]
   end
   def consequence=(new_consequence)
-        @consequence = new_consequence
+        @consequence = new_consequence[]
   end
 end
